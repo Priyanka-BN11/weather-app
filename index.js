b06026720081bf2f33196a1912b7dff4fc618e10
Priyanka-BN11 <78422557+Priyanka-BN11@users.noreply.github.com>
2024-06-28 10:09:35 +0200

    temp of city(munich) with climate icon

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-let request = require('request');
-let apiKey = 'ea712579bc237cf2d06efae53a72fb35';
-let city = 'munich';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+document.addEventListener('DOMContentLoaded', function() {
+   let apiKey = 'ea712579bc237cf2d06efae53a72fb35';
+   let city = 'munich';
+   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-request(url, function(err, response, body){
-    if(err){
-        console.log('error:',error);
-     } else {
-        console.log('body:', body);
-     }
+   fetch(url)
+       .then(response => {
+           if (!response.ok) {
+               throw new Error('Network response was not ok ' + response.statusText);
+           }
+           return response.json();
+       })
+       .then(weather => {
+           let temp = weather.main.temp;
+           let name = weather.name;
+           let iconCode = weather.weather[0].icon;
+           let iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
+           let message = `${temp} degrees Celsius  ${name}!`;
+           
+           document.getElementById('weather').textContent = message;
+           document.getElementById('weather-icon').src = iconUrl;
+
+           console.log(message);
+           console.log(`Weather icon URL: ${iconUrl}`);
+       })
+       .catch(error => {
+           console.error('Fetching error:', error);
+       });
 });
